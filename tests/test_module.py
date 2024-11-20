f20f17fa7b885c032f3410b22f76bae37c1fbed2
Nathan Hui <nthui@ucsd.edu>
2024-11-20 15:40:26 -0800

    feat: Initial code

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,15 +1,15 @@
 """Example test module
 """
-from ansible_deploy_service import example_module
+from ansible_deploy_service import main
 
 def test_example_function():
     """Tests that the example function completes successfully
     """
-    example_module.example_function()
+    main.example_function()
     assert True
 
 def test_example_return():
     """Tests that the function returns something
     """
-    retval = example_module.example_returning_function()
+    retval = main.example_returning_function()
     assert retval is not None
